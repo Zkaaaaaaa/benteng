74140685572b5f61e60e8a00c8c6e3b58f7760d0
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2026-06-04 02:54:26 +0700

    Update allergen-badges.blade.php

--- resources/views/components/allergen-badges.blade.php
+++ resources/views/components/allergen-badges.blade.php
@@ -1,15 +1,3 @@
 @props(['product', 'locale' => 'en'])
 
-@php
-    $badges = $product->activeAllergenBadges($locale);
-@endphp
-
-<span class="allergen-badges" {{ $attributes }}>
-    @if(count($badges) > 0)
-        @foreach($badges as $badge)
-            <span class="allergen-badge" title="{{ $badge['label'] }}">{{ $badge['icon'] }} {{ $badge['label'] }}</span>
-        @endforeach
-    @else
-        <span class="allergen-none">{{ $locale === 'nl' ? 'Geen allergenen' : 'No allergens' }}</span>
-    @endif
-</span>
+{{-- Alergen labels hidden on client menu --}}
